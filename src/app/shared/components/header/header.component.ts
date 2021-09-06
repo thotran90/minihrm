c16826111901620d9ea1adeca30932f2322cc814
ThoTran1990 <trandev90@gmail.com>
2021-09-06 23:28:30 +0700

    initial project

---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  @Output() toogleSideBarForMe: EventEmitter<any> = new EventEmitter();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  toggleSideBar(){
+    this.toogleSideBarForMe.emit();
+  }
+
+}
